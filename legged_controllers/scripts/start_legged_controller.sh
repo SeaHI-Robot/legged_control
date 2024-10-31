116a5b5b51aed01636ec7e97ab9dc44a7ed0968e
Siyuan Wang <12012324@mail.sustech.edu.cn>
2024-10-31 15:48:18 +0800

    reconstructured for only using a1

---
+++ legged_controllers/scripts/start_legged_controller.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+sleep 3
+rosservice call /controller_manager/switch_controller "start_controllers: ['controllers/legged_controller']                   
+stop_controllers: ['']
+strictness: 0
+start_asap: false
+timeout: 0.0" 
+
+
+trot``
---
+++ one_start_gazebo.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+source ../../devel/setup.bash
+
+export ROBOT_TYPE=a1
+
+roslaunch legged_controllers one_start_gazebo.launch
